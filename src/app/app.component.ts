5ef82aeed92171a39a3fdadc8085493761de0c2d
abahuus <abahuus.1979@gmail.com>
2022-02-08 22:53:25 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,10 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
 
-  public message : string="Hello World";
-  public totalPresentase2 : number = 99.999999999;
+  public message : string = "Hello World";
+  public totalPresentase2 : number = 99.99823456;
   public totalPresentase3 : number = 99.199999999;
   public totalPresentase4 : number = 95.1;
-  public totalPresentase5 : number = 99.993;
+  public totalPresentase5 : number = 99.999;
 
 }
