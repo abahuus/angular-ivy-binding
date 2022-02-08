f47c3a42e988cf9fc4540e32549d464bffe9146f
abahuus <abahuus.1979@gmail.com>
2022-02-08 16:06:45 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-d7bckp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-d7bckp)
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+
+  public message : string="Hello World";
+  public totalPresentase2 : number = 99.999999999;
+  public totalPresentase3 : number = 99.199999999;
+  public totalPresentase4 : number = 95.1;
+  public totalPresentase5 : number = 99.993;
+
+}
